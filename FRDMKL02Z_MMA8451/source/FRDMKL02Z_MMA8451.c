26447995307e5d4da847f9f29b0beb528beebc8d
DESKTOP-G71NPO4\Angie Freyle Epieyu <huertassanchezalexander@gmail.com>
2021-01-21 18:24:58 -0500

    Se modifica main.c para realizar ejemplo utilizando la libreria I2C0 y acelerometro MMA8451

--- FRDMKL02Z_MMA8451/source/FRDMKL02Z_MMA8451.c
+++ FRDMKL02Z_MMA8451/source/FRDMKL02Z_MMA8451.c
@@ -41,6 +41,15 @@
 #include "fsl_debug_console.h"
 #include "sdk_hal_gpio.h"
 #include "sdk_hal_uart0.h"
+#include "sdk_hal_i2c0.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define MMA8451_I2C_DEVICE_ADDRESS	0x1D
+
+#define MMA8451_WHO_AM_I_MEMORY_ADDRESS		0x0D
+
+
 /* TODO: insert other include files here. */
 
 /* TODO: insert other definitions and declarations here. */
@@ -51,6 +60,8 @@
 int main(void) {
 	status_t status;
     uint8_t nuevo_byte_uart;
+	uint8_t	nuevo_dato_i2c;
+
   	/* Init board hardware. */
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
@@ -62,12 +73,14 @@ int main(void) {
 
 
     (void)uart0Inicializar(115200);	//115200bps
+    (void)i2c0MasterInit(100000);	//100kbps
 
 
     PRINTF("Usar teclado para controlar LEDs\r\n");
     PRINTF("r-R led ROJO\r\n");
     PRINTF("v-V led VERDE\r\n");
     PRINTF("a-A led AZUL\r\n");
+    PRINTF("M buscar acelerometro\r\n");
 
 
 
@@ -100,6 +113,16 @@ int main(void) {
 					break;
 				case 'R':
 					gpioPutValue(KPTB6,0);
+					break;
+
+				case 'M':
+				i2c0MasterReadByte(&nuevo_dato_i2c, MMA8451_I2C_DEVICE_ADDRESS, MMA8451_WHO_AM_I_MEMORY_ADDRESS);
+
+				if(nuevo_dato_i2c==0x1A)
+			     	printf("MMA8451 encontrado!!\r\n");
+					else
+					printf("MMA8451 error\r\n");
+
 					break;
 
 				}
