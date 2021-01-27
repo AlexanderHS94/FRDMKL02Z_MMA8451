e586454e94f8a83acaac8751621d5e162a26d75a
DESKTOP-G71NPO4\Angie Freyle Epieyu <huertassanchezalexander@gmail.com>
2021-01-27 16:36:39 -0500

    Se implementa la opcion de mostrar los 3 ejes simultanieamente

--- FRDMKL02Z_MMA8451/source/FRDMKL02Z_MMA8451.c
+++ FRDMKL02Z_MMA8451/source/FRDMKL02Z_MMA8451.c
@@ -1,35 +1,7 @@
-/*
- * Copyright 2016-2021 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- 
 /**
  * @file    FRDMKL02Z_MMA8451.c
+ * @author  Alexander Huertas Sanchez
+ * @version V0.2
  * @brief   Application entry point.
  */
 /*  SDK Included Files */
@@ -52,13 +24,6 @@
  * Definitions
  ******************************************************************************/
 #define MMA8451_I2C_DEVICE_ADDRESS	0x1D
-#define MMA8451_WHO_AM_I_MEMORY_ADDRESS		0x0D
-//#define MMA8451_X_MSB_MEMORY_ADDRESS  0x01
-//#define MMA8451_X_LSB_MEMORY_ADDRESS  0x02
-
-
-//#define BOARD_ACCEL_I2C_BASEADDR I2C0
-
 
 
 /* TODO: insert other include files here. */
@@ -106,7 +71,7 @@ int main(void) {
     PRINTF("DATOS DEL ACELEROMETRO EJE X: X-x \r\n");
     PRINTF("DATOS DEL ACELEROMETRO EJE Y: Y-y \r\n");
     PRINTF("DATOS DEL ACELEROMETRO EJE Z: Z-z \r\n");
-
+    PRINTF("DATOS DE LOS EJES (X, Y, Z): T-t \r\n");
     while(1) {
 
     	if(uart0CuantosDatosHayEnBuffer()>0){
